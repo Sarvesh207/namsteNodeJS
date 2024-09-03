4f53469662f6aa27410a6dacef62f8856a4469bf
Sarvesh207 <sarveshgaynar@gmail.com>
2024-09-03 23:33:51 +0530

    initial commit

---
+++ Episode4/app.js
@@ -0,0 +1,5 @@
+require("./xyz");
+const { x, calculateSum } = require("./sum");
+console.log("I am from app.js");
+console.log("Value of x", x);
+console.log("value of y", calculateSum(5,15));
---
+++ Episode4/sum.js
@@ -0,0 +1,7 @@
+function calculateSum (a,b){
+    console.log(`sum is`,a+b)
+    return a+b;
+}
+
+const x = 100;
+module.exports = {x,calculateSum}
